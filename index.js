970090b36ead6b1100f4464083a12c16f23a6694
Anericokakai <challykiddoh@gmail.com>
2023-03-04 13:32:15 +0300

    change animations for back ground

--- index.js
+++ index.js
@@ -1,99 +1,73 @@
-const nav=document.querySelector('nav')
-const ul=nav.querySelector('ul')
-const openBars=nav.querySelector('.fa-bars')
-const closebars=nav.querySelector('.fa-xmark')
+const nav = document.querySelector("nav");
+const ul = nav.querySelector("ul");
+const openBars = nav.querySelector(".fa-bars");
+const closebars = nav.querySelector(".fa-xmark");
 
-
-openBars.onclick=()=>{
-    ul.classList.toggle('menu')
-
-}
-closebars.onclick=()=>{
-    ul.classList.toggle('menu')
-}
+openBars.onclick = () => {
+  ul.classList.toggle("menu");
+};
+closebars.onclick = () => {
+  ul.classList.toggle("menu");
+};
 
 // change the background of the home section
-const homesection=document.querySelector('.homesection')
+const homesection = document.querySelector(".homesection");
+
+var images = [
+  "./images/bgimages/1.jpg",
+  "./images/bgimages/2.jpg",
+  "./images/bgimages/3.jpg",
+  "./images/bgimages/4.jpg",
+  "./images/bgimages/5.jpg",
+  "./images/bgimages/6.jpg",
+  "./images/bgimages/7.jpg",
+  "./images/bgimages/8.jpg",
+  "./images/bgimages/9.jpg",
+  "./images/bgimages/10.jpg",
+  "./images/bgimages/11.jpg",
+];
 
 
-var images=['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg']
-var counter=0
-
-const slider=()=>{
-
-  if(counter<images.length){
-
-    counter++;
-  }else{
-    counter=1
-  }
-
-  homesection.style.background=`linear-gradient(rgba(0, 0, 0, 0.735), rgba(0, 0, 0, 0.74)),url(./images/bgimages/${images[counter-1]})`
-homesection.style.backgroundSize='cover';
-homesection.style.backgroundPosition='center';
-homesection.style.backgroundRepeat='no-repeat;'
-}
-
-setInterval(slider,5000)
 
 // change the navigation to sticky on scroll
 
+var prevoius = window.pageYOffset;
+window.onscroll = () => {
+  var current = window.pageYOffset;
 
-var prevoius=window.pageYOffset
-window.onscroll=()=>{
-
-
-  var current=window.pageYOffset
-
-  if(prevoius>current){
-
-
-    nav.style.top='0'
-    document.querySelector('.image').style.backgroundColor='white'
-    nav.style.transition='1s'
-   
-    
-  }else{
-
-    nav.style.top='-70px'
-    
+  if (prevoius > current) {
+    nav.style.top = "0";
+    document.querySelector(".image").style.backgroundColor = "white";
+    nav.style.transition = "1s";
+  } else {
+    nav.style.top = "-70px";
   }
-  prevoius=current
-}
+  prevoius = current;
+};
 
 // pre;loader
-var preloader=document.querySelector('#preloader')
+var preloader = document.querySelector("#preloader");
 
-const preloaderfunction=()=>{
-
-  preloader.style.display='none'
-}
-window.addEventListener('load',preloaderfunction)
-
-
-
-
-
+const preloaderfunction = () => {
+  preloader.style.display = "none";
+};
+window.addEventListener("load", preloaderfunction);
 
 // smooth scroll
 
-const observer=new IntersectionObserver((entries)=>{
-    entries.forEach(entry =>{
-      
-      if(entry.isIntersecting){
-        entry.target.classList.add('show')
-      }else{
-        entry.target.classList.remove('show')
-      }
-    })
-  })
-  
-  
-  const hidden=document.querySelectorAll('.hidden')
-  hidden.forEach(el =>{
-    observer.observe(el)
-  })
-  
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-  const page=window.location
-  
+const hidden = document.querySelectorAll(".hidden");
+hidden.forEach((el) => {
+  observer.observe(el);
+});
+
+const page = window.location;
